c218f3991812f99d0c051b010fbd8ded963af542
Pankaj Jangid - N <pankajexpressian@gmail.com>
2022-04-09 23:59:25 +0530

    Moved class to dedicated file

--- src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
+++ src/Services/Ordering/Ordering.Application/Exceptions/NotFoundException.cs
@@ -1,7 +1,4 @@
-﻿using FluentValidation.Results;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,23 +11,6 @@ namespace Ordering.Application.Exceptions
             : base($"Entity \"{name}\" ({key}) was not found.")
         {
 
-        }
-    }
-
-    public class ValidationException : ApplicationException
-    {
-        public IDictionary<string, string[]> Errors { get; }
-        public ValidationException()
-            : base($"One or more validation failure occurred.")
-        {
-            Errors = new Dictionary<string, string[]>();
-        }
-
-        public ValidationException(IEnumerable<ValidationFailure> failures)
-            : this()
-        {
-            Errors = failures.GroupBy(e => e.PropertyName, e => e.ErrorMessage)
-                .ToDictionary(fg => fg.Key, fg => fg.ToArray());
         }
     }
 
