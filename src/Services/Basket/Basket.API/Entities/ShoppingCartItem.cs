8f48ef0ac343f92f57a4213a9e5ae24745e1ecbe
pankajexpressian <pankajexpressian@gmail.com>
2022-05-03 18:54:17 +0530

    Added and containerized the Shopping Aggregator service.

--- src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -5,7 +5,7 @@
         public int Quantity { get; set; }
         public string Color { get; set; }
         public decimal Price { get; set; }
-        public string PorudctId { get; set; }
+        public string ProductId { get; set; }
         public string ProductName { get; set; }
     }
 }
